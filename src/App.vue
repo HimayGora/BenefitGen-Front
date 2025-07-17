c52245f68d488391b7b84c450a57268f03d3c706
Himay Gora <himay200@gmail.com>
2025-07-17 11:26:24 -0400

    fix(app.vue): add missing import and fix typos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import { useAuth } from './store/auth';
+import TheHeader from './components/TheHeader.vue';
 
 const { checkAuthStatus } = useAuth();
 const isReady = ref(false);
@@ -16,7 +17,7 @@ onMounted(async () => {
     <p class="text-white">Loading...</p>
   </div>
   <div v-else>
-    <AppHeader />
+    <TheHeader />
     <RouterView />
   </div>
 </template>
