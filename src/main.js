fd0c828cddbeabc214176c6dfe9922e53ca51c09
Himay Gora <himay200@gmail.com>
2025-07-21 14:47:17 -0400

    'fix(main.js): add router instance to gtag for SPA tracking'

--- src/main.js
+++ src/main.js
@@ -6,12 +6,11 @@ import App from './App.vue'
 import './style.css'
 import { createGtag } from 'vue-gtag'; // Correct import for vue-gtag v3.x
 const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
-console.log('GA Measurement ID:', measurementId)
 const head = createHead()
 const gtag = createGtag({
   config: { id: measurementId}
   
-})
+}, router)
 
 createApp(App)
   .use(head)
